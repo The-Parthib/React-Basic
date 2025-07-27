bdf147cffd73e46fd24718ea8aeeee1015d3d664
The-Parthib <rijupanja81@gmail.com>
2025-07-28 00:10:18 +0530

    feat: integrate Tailwind CSS and implement Todo functionality
    - Added Tailwind CSS and Vite plugin for styling.
    - Created AddTodo component for adding new todos.
    - Created Todos component for displaying, editing, and removing todos.
    - Implemented Redux slice for managing todo state with add, remove, and update actions.
    - Updated App component to include AddTodo and Todos components.
    - Configured Redux store to use the todo slice reducer.
    - Refactored main entry point to provide Redux store to the application.

--- Projects/Redux/ReduxToolkitTodo/src/features/todo/todoSlice.js
+++ Projects/Redux/ReduxToolkitTodo/src/features/todo/todoSlice.js
@@ -1,1 +1,38 @@
-import { createSlice,nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      text: "Hello world",
+    },
+  ],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState, // initialState: initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload, // text: action.payload.text
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      const { id, text } = action.payload;
+      const existingTodo = state.todos.find((todo) => todo.id === id);
+      if (existingTodo) {
+        existingTodo.text = text;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
